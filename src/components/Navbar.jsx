5ec6e6389d90aee5add0cb0744d25b1e9b6dc2ff
Eifro <afrotricks19@gmail.com>
2022-09-24 10:19:29 -0500

    navbar end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
         <div className='z-10'>
           <h1 className="md:text-3xl text-2xl  font-bold">◄►</h1>
         </div>
-        <ul className={`md:flex md:static md:gap-12 md:pt-0 md:w-max md:h-max fixed top-0 left-0 w-full h-screen text-center pt-32 bg-black`}>
+        <ul className={`md:flex md:static md:gap-12 md:pt-0 md:w-max md:h-max fixed top-0 left-0 w-full h-screen text-center pt-32 bg-black opacity-90 ${open ? "" : "hidden"}`}>
           {links.map((link) => (
             <li key={link.name} className='font-medium duration-300 mt-10 hover:scale-125 md:mt-0 md:hover:scale-110 hover:font-bold hover:text-indigo-400 md:hover:border-b md:hover:border-b-indigo-400'>
               <a href={link.link}>{link.name}</a>
