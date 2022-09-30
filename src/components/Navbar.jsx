f18124d7ff8bac3aaa4f94b00f5588a0db6aa082
Eifro <afrotricks19@gmail.com>
2022-09-30 11:33:55 -0500

    navbar end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+
 /* icons */
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
   /* states */
-  const [open, setOpen] = useState(false)
+  const [sticky, setSticky] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const links = [
     { name: "HOME", link: "#" },
@@ -14,23 +16,35 @@ const Navbar = () => {
     { name: "CONTACT", link: "#" },
   ];
 
+  useEffect(() => {
+    window.onscroll = () => {
+      const scroll = document.documentElement.scrollTop
+      if (scroll > 30) setSticky(true)
+      else setSticky(false)
+    }
+  }, [])
+
   return (
-    <div className="w-full bg-black text-white fixed">
-      <div className="h-20 px-4 container mx-auto flex justify-between items-center">
-        <div className='z-10'>
-          <h1 className="md:text-3xl text-xl font-bold">MiguelWeb</h1>
+    <div className="w-full fixed">
+      <nav className={`md:py-0 py-4 flex justify-between items-center transition ease-linear ${sticky ? 'bg-white/70 text-gray-900' : 'text-white'}`}>
+        <div className="mx-4">
+          <h1 className="md:text-3xl lg:text-4xl text-2xl font-bold">
+            Mi<span className="text-emerald-500">guelW</span>eb
+          </h1>
         </div>
-        <ul className={`md:flex md:static md:gap-12 md:pt-0 md:w-max md:h-max fixed top-0 left-0 w-full h-screen text-center pt-32 bg-black opacity-90 ${open ? "" : "hidden"}`}>
-          {links.map((link) => (
-            <li key={link.name} className='font-medium duration-300 mt-10 hover:scale-125 md:mt-0 md:hover:scale-110 hover:font-bold hover:text-indigo-400 md:hover:border-b md:hover:border-b-indigo-400'>
-              <a href={link.link}>{link.name}</a>
-            </li>
-          ))}
-        </ul>
-        <div className='md:hidden cursor-pointer z-10' onClick={() => {setOpen(!open)}}>
-          {open ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+        <div className={`md:z-0 md:block md:static md:h-max md:w-max md:bg-white md:py-4 md:px-14 md:rounded-bl-full fixed top-0 left-0 w-full h-screen bg-gray-900 -z-10 ${open ? "hidden" : ""}`}>
+          <ul className="md:flex-row flex flex-col items-center justify-center gap-14 h-full">
+            {links.map((link, key) => (
+              <li key={key} className="md:text-lg md:hover:scale-105 font-bold md:font-medium text-xl text-white md:text-gray-900 hover:text-emerald-500 hover:scale-110 transition ease-linear duration-300">
+                <a href={link.link}>{link.name}</a>
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
+        <div className="md:hidden cursor-pointer mx-4" onClick={() => setOpen(!open)}>
+          {open ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
+        </div>
+      </nav>
     </div>
   );
 };
